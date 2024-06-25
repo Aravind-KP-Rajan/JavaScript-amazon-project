d4e24d1ae0744e43c4914fd46242abe355d61380
Aravind-KP-Rajan <aravind777kp@gmail.com>
2024-06-25 19:53:30 +0530

    Regenerate HTML for Order summary.

--- Scripts/checkout/orderSummary.js
+++ Scripts/checkout/orderSummary.js
@@ -141,11 +141,10 @@ export function renderOrderSummary (){
     const productId = link.dataset.productId;
     removeFromCart(productId);
 
-    const container = document.querySelector(`.js-cart-item-container-${productId}`
-    );
-
-    container.remove();
+  
     updateCartQuantity();
+
+    renderOrderSummary();
 
     renderPaymentSummary(); //regenerating the data
 
