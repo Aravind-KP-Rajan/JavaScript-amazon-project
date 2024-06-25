d6d3c86f21d25a4546fca172dcab7f92841ceb5f
Aravind-KP-Rajan <aravind777kp@gmail.com>
2024-06-25 19:54:08 +0530

    update Item quantity in payment summary.

--- Scripts/checkout/paymentSummary.js
+++ Scripts/checkout/paymentSummary.js
@@ -20,8 +20,13 @@ export function renderPaymentSummary(){
 
   const totalBeforeTaxCents = productPriceCents + shippingPriceCents;
   const taxCents = totalBeforeTaxCents * 0.1;
-
   const totalCents = totalBeforeTaxCents + taxCents;
+
+  let cartQuantity = 0;
+
+  cart.forEach((cartItem) => {
+    cartQuantity += cartItem.quantity;
+  });
 
 
   const paymentSummaryHTML = `
@@ -30,7 +35,7 @@ export function renderPaymentSummary(){
             </div>
 
             <div class="payment-summary-row">
-              <div>Items (3):</div>
+              <div>Items (${cartQuantity}):</div>
               <div class="payment-summary-money">
               $${formatCurrency(productPriceCents)}
               </div>
